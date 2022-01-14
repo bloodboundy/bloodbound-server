1f38b1c7c5c724635d7481923b2d04e1ad174595
TWT233 <TWT2333@outlook.com>
2022-01-15 00:27:35 +0800

    feat(player): init

---
+++ player/player.go
@@ -0,0 +1,45 @@
+package player
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Player struct {
+	mu   *sync.Mutex
+	ID   string
+	game string
+}
+
+func NewPlayer(id string, game string) *Player {
+	return &Player{
+		mu:   &sync.Mutex{},
+		ID:   id,
+		game: game,
+	}
+}
+
+func (p *Player) Join(game string) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if p.game != "" {
+		return fmt.Errorf("already in game")
+	}
+	p.game = game
+	return nil
+}
+
+func (p *Player) Leave() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if p.game == "" {
+		return
+	}
+	p.game = ""
+}
+
+func (p *Player) Game() string {
+	return p.game
+}
