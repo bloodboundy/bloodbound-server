e2c59fb29eafaad54eb424b959880601f409098e
TWT233 <TWT2333@outlook.com>
2022-01-19 22:02:24 +0800

    refactor: use gin rather than net/http

--- environment.go
+++ environment.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"context"
 	"sync/atomic"
 
 	"github.com/bloodboundy/bloodbound-server/game"
 	"github.com/bloodboundy/bloodbound-server/player"
+	"github.com/bloodboundy/bloodbound-server/ws"
+	"github.com/gin-gonic/gin"
 )
 
 var (
 	gameManager   atomic.Value
 	playerManager atomic.Value
+	wsManager     atomic.Value
 )
 
 func init() {
 	gameManager.Store(game.NewManager())
 	playerManager.Store(player.NewManager())
-
+	wsManager.Store(ws.NewManager())
 }
 
-func mixServerCtx(ctx context.Context) context.Context {
-	ctx = game.MixManager(ctx, gameManager.Load().(*game.Manager))
-	ctx = player.MixManager(ctx, playerManager.Load().(*player.Manager))
-	return ctx
+func mixManagers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := c.Request.Context()
+
+		ctx = game.MixManager(ctx, gameManager.Load().(*game.Manager))
+		ctx = player.MixManager(ctx, gameManager.Load().(*player.Manager))
+		ctx = ws.MixManager(ctx, wsManager.Load().(*ws.Manager))
+
+		c.Request = c.Request.WithContext(ctx)
+
+		c.Next()
+	}
 }
