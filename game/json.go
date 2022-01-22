2d020d6220ad6d5729c3771c8be3420a0a658945
TWT233 <TWT2333@outlook.com>
2022-01-22 19:55:20 +0800

    refactor: move load/dump

--- game/json.go
+++ game/json.go
@@ -1,6 +1,10 @@
 package game
 
-import "github.com/bloodboundy/bloodbound-server/player"
+import (
+	"github.com/bloodboundy/bloodbound-server/player"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/proto"
+)
 
 // GameJSON represents the JSON format of a game, used to communicate
 type GameJSON struct {
@@ -16,4 +20,38 @@ type GameJSON struct {
 
 	// contained data
 	Players []*player.Player `json:"players,omitempty"`
+}
+
+// Load settings from `src`
+func (g *Game) Load(src *GameJSON) error {
+	if err := g.SetMaxPlayers(src.MaxPlayers); err != nil {
+		return errors.Wrap(err, "setMaxPlayers")
+	}
+	g.password = src.Password
+	return nil
+}
+
+// Dump game to GameJSON
+//
+// normally, the "password", "players" were filter out from ret-val
+// include it in addition to add it into ret-val
+func (g *Game) Dump(addition ...string) *GameJSON {
+	gj := &GameJSON{
+		ID:         g.id,
+		MaxPlayers: proto.Uint32(g.maxPlayers),
+		IsPrivate:  g.IsPrivate(),
+		CreatedAt:  g.createdAt,
+		CreatedBy:  g.createdBy,
+	}
+
+	for _, field := range addition {
+		switch field {
+		case "password":
+			gj.Password = g.password
+		case "players":
+			gj.Players = g.ListPlayers()
+		}
+	}
+
+	return gj
 }
--- player/json.go
+++ player/json.go
@@ -4,3 +4,10 @@ type PlayerJSON struct {
 	ID       string `json:"id,omitempty"`
 	Nickname string `json:"nickname,omitempty"`
 }
+
+func (p *Player) Dump() *PlayerJSON {
+	return &PlayerJSON{
+		ID:       p.id,
+		Nickname: p.nickname,
+	}
+}
