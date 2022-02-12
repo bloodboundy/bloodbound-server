4c1098697a440017fcd2b74c76439869831d3803
TWT <TWT2333@outlook.com>
2022-02-12 14:15:27 +0800

    refactor(action): include op.index into comm

--- action/ask_int.go
+++ action/ask_int.go
@@ -15,35 +15,28 @@ func init() {
 		AskIntACT, AskIntActionJSON{},
 		func(ctx context.Context, state *game.State, jsi interface{}) (Action, error) {
 			jso := jsi.(*AskIntActionJSON)
-			return &AskIntAction{
-				actionComm: jso.makeActionComm(TargetACT),
-				from:       jso.From,
-			}, nil
+			return &AskIntAction{jso.makeActionComm(TargetACT)}, nil
 		})
 }
 
 type AskIntAction struct {
 	actionComm
-
-	from uint32
 }
 
 type AskIntActionJSON struct {
 	actionJSONComm
-	From     uint32 `json:"from"`
 	Attacker uint32 `json:"attacker"`
 }
 
 func (a *AskIntAction) Dump(ctx context.Context, state *game.State) *AskIntActionJSON {
 	return &AskIntActionJSON{
 		actionJSONComm: a.makeActionJSONComm(state),
-		From:           a.from,
 		Attacker:       state.DaggerIn,
 	}
 }
 
 func (a *AskIntAction) Check(ctx context.Context, state *game.State) error {
-	if state.DaggerTarget != a.from {
+	if state.DaggerTarget != a.index {
 		return errors.Errorf("not dagger target, current dagger target is %d", state.DaggerTarget)
 	}
 	return nil
